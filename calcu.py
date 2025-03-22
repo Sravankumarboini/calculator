640fa9af6edeeb6a8901f7415f3f95cc712e5794
Your Name <sravankumarboini005.com>
2025-03-22 10:12:50 +0530

    python calci interface

---
+++ calcu.py
@@ -0,0 +1,57 @@
+import math
+
+def get_input():
+    """Function to take input from the user."""
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    return a, b
+
+def add(a, b):
+    pass  # To be implemented by a team member
+
+def subtract(a, b):
+    pass  # To be implemented by a team member
+
+def multiply(a, b):
+    pass  # To be implemented by a team member
+
+def divide(a, b):
+    pass  # To be implemented by a team member
+
+def modulus(a, b):
+    pass  # To be implemented by a team member
+
+def power(a, b):
+    pass  # To be implemented by a team member
+
+def square_root(a):
+    pass  # To be implemented by a team member
+
+def factorial(a):
+    pass  # To be implemented by a team member
+
+def sine(a):
+    pass  # To be implemented by a team member
+
+def cosine(a):
+    pass  # To be implemented by a team member
+
+def tangent(a):
+    pass  # To be implemented by a team member
+
+def logarithm(a, b):
+    pass  # To be implemented by a team member
+
+def exponent(a):
+    pass  # To be implemented by a team member
+
+def absolute(a):
+    pass  # To be implemented by a team member
+
+def calculator():
+    print("\nWelcome to the Team Calculator!")
+    a, b = get_input()  # Taking input for `a` and `b`
+    print(f"Values received: a = {a}, b = {b}")
+
+if __name__ == "__main__":
+    calculator()
